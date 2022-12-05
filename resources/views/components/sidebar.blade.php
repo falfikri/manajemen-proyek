d0d0f6ed17cb866ba6ada28ac21773ac2bc7ed1a
falfikri <fazliialfikri@gmail.com>
2022-12-05 22:20:35 +0700

    Hapus button log-out dan gambarnnya

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -31,33 +31,5 @@
           <span class="menu-title">Progres Terhenti</span>
         </a>
       </li>
-
-
-      <li class="nav-item sidebar-user-actions">
-        <div class="user-details">
-          <div class="d-flex justify-content-between align-items-center">
-            <div>
-              <div class="d-flex align-items-center">
-                <div class="sidebar-profile-img">
-                  <img src="{{ asset('assets/images/faces/face28.png') }}" alt="image">
-                </div>
-                <div class="sidebar-profile-text">
-                  <p class="mb-1">
-
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
-      
-      
-      <li class="nav-item sidebar-user-actions">
-        <div class="sidebar-user-menu">
-          <a href="#" class="nav-link"><i class="mdi mdi-logout menu-icon"></i>
-            <span class="menu-title">Log Out</span></a>
-        </div>
-      </li>
     </ul>
   </nav>
